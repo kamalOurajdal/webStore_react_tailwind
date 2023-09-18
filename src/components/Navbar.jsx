d426d08130933f8e9d7cad5cc46a76a5db280a00
kamal <kamalourajdal@gmail.com>
2023-09-18 19:59:00 +0100

    login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,101 @@
-import React, { useState } from "react";
-import { Collapse } from "react-collapse";
+import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function Navbar() {
-  const [showNavLinks, setShowNavLinks] = useState(false);
-  const toggleShowNavLinks = () => {
-    setShowNavLinks(!showNavLinks);
-  };
-
   const productDropdown = (
-    <div className={`${showNavLinks ? "h-[180px]":"h-0  py-0"} py-2 top-8 transform-w duration-500 ease-in-out overflow-hidden  absolute z-40 text-sm  bg-white  space-y-4 w-48  rounded-lg shadow-md text-gray-600`}>
-      <Link
-        to="/products/new"
-        className="flex items-center justify-between px-4  hover:text-rose-500"
-      >
-        New products <i class="fa-solid fa-caret-right"></i>
-      </Link>
-      <Link
-        to="/products/top-sellers"
-        className="flex items-center justify-between px-4  hover:text-rose-500"
-      >
-        Our top sellers <i class="fa-solid fa-caret-right"></i>
-      </Link>
-      <Link
-        to="/products/limited-edition"
-        className="flex items-center justify-between px-4  hover:text-rose-500"
-      >
-        Limited edition <i class="fa-solid fa-caret-right"></i>
-      </Link>
-      <Link
-        to="/products/last-chance"
-        className="flex items-center justify-between px-4  hover:text-rose-500"
-      >
-        Last chance to buy <i class="fa-solid fa-caret-right"></i>
-      </Link>
-      <Link
-        to="/products/trending"
-        className="flex items-center justify-between px-4  hover:text-rose-500"
-      >
-        Trending <i class="fa-solid fa-caret-right"></i>
-      </Link>
+    <div
+      className={`h-0 py-0 group-hover:block group-hover:h-[168px] top-5 transform-w duration-500 ease-in-out overflow-hidden  absolute z-40 text-xs bg-white   w-48  rounded-lg shadow-md text-gray-600`}
+    >
+      <div className="w-full space-y-4 pt-4">
+        <Link
+          to="/products/new"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          New products <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/products/top-sellers"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Our top sellers <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/products/limited-edition"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Limited edition <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/products/last-chance"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Last chance to buy <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/products/trending"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Trending <i class="fa-solid fa-caret-right"></i>
+        </Link>
+      </div>
+    </div>
+  );
+  const dealDropdown = (
+    <div
+      className={`h-0 py-0 group-hover:block group-hover:h-[140px] top-5 transform-w duration-500 ease-in-out overflow-hidden  absolute z-40 text-xs  bg-white   w-48  rounded-lg shadow-md text-gray-600`}
+    >
+      <div className="w-full space-y-4 pt-4 ">
+        <Link
+          to="/deals/lowest"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          The Lowest price <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/deaks/under?5"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Solutions under 5$ <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/deaks/under?7.5"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Solutions under 7.5$ <i class="fa-solid fa-caret-right"></i>
+        </Link>
+        <Link
+          to="/deaks/under?10"
+          className="flex items-center justify-between px-4  hover:text-rose-500"
+        >
+          Solutions under 10$ <i class="fa-solid fa-caret-right"></i>
+        </Link>
+      </div>
     </div>
   );
 
   return (
-    <nav className="w-full shadow-md relative">
-      <div className="mr-8 flex justify-end items-center h-16 bg-white text-black">
+    <nav className="w-full shadow-md relative h-14 text-sm flex flex-col justify-center ">
+      <div className="w-[95%] flex justify-end items-center  text-black">
         <Link to="/" className="mx-4 hover:text-rose-500">
           Home
         </Link>
-        <div className="relative " onClick={() => toggleShowNavLinks()}> 
-          <Link to="/" className="mx-4 hover:text-rose-500 ">
-            Products <i class={`${showNavLinks ? "rotate-180" : ""} fa-solid fa-caret-up ml-1 transform duration-300 ease-in-out`}></i>
-          </Link>
-              {productDropdown}
+        <div className="relative group">
+          <p className="mx-4 group-hover:text-rose-500 cursor-pointer ">
+            Products{" "}
+            <i
+              class={`group-hover:rotate-180 fa-solid fa-caret-up ml-1 transform duration-300 ease-in-out`}
+            ></i>
+          </p>
+          {productDropdown}
+        </div>
+        <div className="relative group">
+          <p className="mx-4 group-hover:text-rose-500 cursor-pointer ">
+            Deals{" "}
+            <i
+              class={`group-hover:rotate-180 fa-solid fa-caret-up ml-1 transform duration-300 ease-in-out`}
+            ></i>
+          </p>
+          {dealDropdown}
         </div>
         <Link to="/about" className="mx-4 hover:text-rose-500">
           About
@@ -61,7 +103,7 @@ function Navbar() {
         <Link to="/contact" className="mx-4 hover:text-rose-500">
           Contact
         </Link>
-        <Link to="/help" className="mx-4 hover:text-rose-500">
+        <Link to="/help" className="ml-4 hover:text-rose-500">
           Help
         </Link>
         <Outlet />
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Search() {
   window.addEventListener("scroll", function () {
@@ -13,41 +13,51 @@ function Search() {
 
   return (
     <section
-      className={` header w-full flex items-center justify-around py-4 bg-white`}
+      className={` header w-full flex flex-col items-center justify-around px-16 pt-4 pb-2 bg-white`}
     >
-      <h1 className="text-3xl font-bold">
-        Kan<span className="text-[#e94560]">9ala</span>
-      </h1>
-      <div className=" flex items-center bg-[#F5F5F5] rounded-full pl-4 w-1/2 ">
-        <i className="fa-solid fa-magnifying-glass text-gray-500"></i>
-        <input
-          type="text"
-          name=""
-          id=""
-          placeholder="Type to search products..."
-          className="  h-11 bg-transparent rounded-full px-4 focus:outline-none placeholder:text-sm w-full"
-        />
-      </div>
-
-      <div className="  flex  items-center justify-between space-x-8 text-gray-500">
-        <Link to={"/card"}>
-          <div className="relative cursor-pointer">
-          <i class="fa-solid fa-cart-shopping text-2xl"></i>
-            <span className="absolute -right-2 -top-1 text-xs w-4 text-center  bg-red-500 rounded-full text-white font-semibold">
-              0
-            </span>
+      <div className="w-full flex items-center justify-around ">
+        <h1 className="text-3xl font-bold">
+          Kan<span className="text-[#e94560]">9ala</span>
+        </h1>
+        <div className="flex-grow mx-12 ">
+          <div className=" flex items-center bg-[#F5F5F5] rounded-full pl-4 w-full ">
+            <i className="fa-solid fa-magnifying-glass text-gray-500"></i>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Type to search products..."
+              className="w-full h-11 bg-transparent rounded-full px-4 focus:outline-none placeholder:text-sm "
+            />
           </div>
-        </Link>
-        <i class="fa-regular fa-heart text-2xl cursor-pointer"></i>
-
-        <div className="flex justify-between space-x-6 ">
-          <button className="text-sm font-semibold text-black hover:text-gray-700 border  px-3 py-[7px] rounded ">
-            Sign in
-          </button>
-          <button className="text-sm font-semibold text-white bg-orange-400 px-3 py-[7px] rounded">
-            Register
-          </button>
         </div>
+
+        <div className="  flex  items-center justify-between space-x-8 text-gray-500">
+          <Link to={"/card"}>
+            <div className="relative cursor-pointer">
+              <i class="fa-solid fa-cart-shopping text-xl"></i>
+              <span className="absolute -right-2 -top-1 text-[9px] w-4 h-4 flex items-center justify-center  bg-[#e94560] rounded-full text-white font-semibold">
+                2
+              </span>
+            </div>
+          </Link>
+          <Link to={"/wish_list"}><i class="fa-regular fa-heart text-xl cursor-pointer"></i></Link>
+          <Link to={"profile/login"}><i class="fa-regular fa-user text-xl cursor-pointer"></i></Link>
+          
+        </div>
+      </div>
+      <div>
+        <ul className="flex items-center space-x-6 text-gray-500 text-xs mt-2 ml-4">
+          <li className="cursor-pointer hover:text-[#e94560] ">iphone case</li>
+          <li className="cursor-pointer hover:text-[#e94560]">watch for men</li>
+          <li className="cursor-pointer hover:text-[#e94560]">
+            sunglasses women
+          </li>
+          <li className="cursor-pointer hover:text-[#e94560]">
+            makeup brushes
+          </li>
+          <li className="cursor-pointer hover:text-[#e94560]">massage gun</li>
+        </ul>
       </div>
     </section>
   );
